7f689997267b2d6b798e47328e49cf6fa787d5cc
4NM0U5 <65267991+ANONYMOUS-00700@users.noreply.github.com>
2020-08-18 13:12:58 +0530

    Add files via upload

---
+++ ANMOUS_PRANK.py
@@ -0,0 +1,25 @@
+#don't forget to leave a star 
+#you can see its really easy to make a specific output in python 
+
+print("\033[0;35;40m     _    _   _ __  __  ___  _   _ ____\n")
+print("\033[0;35;40m    / \  | \ | |  \/  |/ _ \| | | / ___| \n")
+print("\033[0;35;40m   / _ \ |  \| | |\/| | | | | | | \___ \ \n")
+print("\033[0;35;40m  / ___ \| |\  | |  | | |_| | |_| |___) |\n")
+print("\033[0;35;40m /_/   \_\_| \_|_|  |_|\___/ \___/|____/\n")
+
+print("v.1.0....coded by \033[1;31;40m 4NM0U5 \n")
+
+print("ANMOUS")
+
+print("1.HACK")
+print("2.EXIT")
+
+while True:
+            
+            choice = input("What you want to do ::")
+             
+            if choice == '1':
+                 print("DO IT YOURESELF!!! :-)")
+            
+            if choice == '2':
+                 print("press ctrl+C" )
